cd267a0fc06fffa84b716eb6007d0fe40222d71b
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-12 15:44:04 +0900

    Feat : chap02(Variable)_입출력 #2 - 화면입력

--- chap02/Variable.java
+++ chap02/Variable.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // 1. 입출력
 class Variable1 {
     public static void main(String[] args) {
@@ -10,5 +12,22 @@ class Variable2 {
     public static void main(String[] args) {
         System.out.println("Hello, Mallang!");
         System.out.println("It's Java world.");
+    }
+}
+
+class Variable3 {
+    public static void main(String[] args) {
+        // Scanner 클래스 객체 생성
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("두 자리 정수를 입력하라 : ");
+
+        // 입력내용 -> input 변수에 저장 (string type)
+        String input = scanner.nextLine();
+        // string type input변수 -> int type으로 변환
+        int num = Integer.parseInt(input);
+
+        System.out.println("입력내용 : " + input);
+        System.out.printf("num = %d%n", num);
     }
 }
