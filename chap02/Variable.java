8084e327f370061f0fce2a4744cda4dfb52b9a80
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-12 13:44:11 +0900

    Feat : chap02(Variable)_입출력 #2 - 화면출력

---
+++ chap02/Variable.java
@@ -0,0 +1,14 @@
+// 1. 입출력
+class Variable1 {
+    public static void main(String[] args) {
+        System.out.print("Hello, Mallang!");
+        System.out.print("It's Java world.");
+    }
+}
+
+class Variable2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mallang!");
+        System.out.println("It's Java world.");
+    }
+}
