0fe31f19cc328ef7ae2f8fe69549666edc62ec35
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-13 18:27:50 +0900

    Feat : chap02(Variable)_변수 #3 - 타입간변환

--- chap02/Variable.java
+++ chap02/Variable.java
@@ -97,3 +97,21 @@ class Variable7 {
         System.out.println("y = " + y);
     }
 }
+
+// 6. 타입간변환
+class Variable8 {
+    public static void main(String[] args) {
+        String str = "3";
+
+        // 문자열 -> 문자 -> 숫자
+        System.out.println(str.charAt(0));
+        // 문자 -> 숫자
+        System.out.println('3' - '0'+ 1);
+        // 문자열 -> 숫자
+        System.out.println(Integer.parseInt("3") + 1);
+        // 문자열 + 문자열
+        System.out.println("3" + 1);
+        // 숫자 -> 문자
+        System.out.println((char) (3 + '0'));
+    }
+}
