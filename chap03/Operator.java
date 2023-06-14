5a234c6f000b996d9ab49e919e8cd89efe1f0a84
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-14 18:56:23 +0900

    Feat : chap03(Operator)_연산자 #4 - 증감연산자

--- chap03/Operator.java
+++ chap03/Operator.java
@@ -10,3 +10,22 @@ class Operator1 {
         System.out.println("y = " + y);
     }
 }
+
+// 2. 증감연산자
+class Operator2 {
+    public static void main(String[] args) {
+        int i = 5, j = 0;
+
+        // 후위형
+        j = i++;
+        System.out.println("j = i++; 실행 후, i = " + i + " j = " + j);
+
+        // 값 초기화
+        i = 5;
+        j = 0;
+
+        // 전위형
+        j = ++i;
+        System.out.println("j = ++i; 실행 후, i = " + i + " j = " + j);
+    }
+}
