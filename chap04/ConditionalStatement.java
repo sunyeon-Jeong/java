68de5afa0134d7289a41ab151e3c289553f0a5ae
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-15 17:48:12 +0900

    Feat : chap04(ConditionalStatement)_조건문 #5 - 다양한조건식

--- chap04/ConditionalStatement.java
+++ chap04/ConditionalStatement.java
@@ -8,3 +8,24 @@ class ConditionalStatement1 {
         }
     }
 }
+
+// 2. 다양한조건식
+class ConditionalStatement2 {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x = %d일때, 참인 것은%n", x);
+
+        if (x == 0) System.out.println("x == 0");
+        if (x != 0) System.out.println("x != 0");
+        if (! (x == 0)) System.out.println("! (x == 0)");
+        if (! (x != 0)) System.out.println("! (x != 0)");
+
+        x = 1;
+        System.out.printf("x = %d일때, 참인 것은%n", x);
+
+        if (x == 0) System.out.println("x == 0");
+        if (x != 0) System.out.println("x != 0");
+        if (! (x == 0)) System.out.println("! (x == 0)");
+        if (! (x != 0)) System.out.println("! (x != 0)");
+    }
+}
