53a7ab72bde909831c12b4362693e285c81fbd01
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-15 18:31:37 +0900

    Feat : chap04(ConditionalStatement)_조건문 #5 - 중첩if문

--- chap04/ConditionalStatement.java
+++ chap04/ConditionalStatement.java
@@ -73,3 +73,37 @@ class ConditionalStatement4 {
         System.out.println("당신의 학점은 " + grade + "입니다.");
     }
 }
+
+// 5. 중첩 if문
+class ConditionalStatement5 {
+    public static void main(String[] args) {
+        int score = 0;
+        char grade = ' ', opt = '0';
+
+        System.out.print("점수를 입력하시오 >>> ");
+        Scanner scanner = new Scanner(System.in);
+        score = scanner.nextInt();
+
+        if (score >= 90) {
+            grade = 'A';
+
+            if (score >= 98) {
+                opt = '+';
+            } else if (score < 94) {
+                opt = '-';
+            }
+        } else if (score >= 80) {
+            grade = 'B';
+
+            if (score >= 88) {
+                opt = '+';
+            } else if (score < 84) {
+                opt = '-';
+            }
+        } else {
+            grade = 'C';
+        }
+
+        System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
+    }
+}
