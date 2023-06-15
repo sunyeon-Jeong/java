da6ccc8271b16348193973e17f79b30559a5595c
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-15 18:04:41 +0900

    Feat : chap04(ConditionalStatement)_조건문 #5 - if-else문

--- chap04/ConditionalStatement.java
+++ chap04/ConditionalStatement.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // 1. if문
 class ConditionalStatement1 {
     public static void main(String[] args) {
@@ -27,5 +29,23 @@ class ConditionalStatement2 {
         if (x != 0) System.out.println("x != 0");
         if (! (x == 0)) System.out.println("! (x == 0)");
         if (! (x != 0)) System.out.println("! (x != 0)");
+    }
+}
+
+// 3. if-else문
+class ConditionalStatement3 {
+    public static void main(String[] args) {
+        System.out.print("숫자를 하나 입력하시오 >>>");
+
+        // 사용자입력값 받기 위한 객체생성
+        Scanner scanner = new Scanner(System.in);
+        // 사용자입력값 -> int타입으로 변수에저장
+        int input = scanner.nextInt();
+
+        if (input == 0) {
+            System.out.println("입력하신 숫자는 0입니다.");
+        } else {
+            System.out.println("입력하신 숫자는 0이 아닙니다.");
+        }
     }
 }
