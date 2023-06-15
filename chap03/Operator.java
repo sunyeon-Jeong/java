febfbc425c9361059c19832b84c2a75b3c81c7fc
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-15 14:42:17 +0900

    Feat : chap03(Operator)_연산자 #4 - Math.round()반올림

--- chap03/Operator.java
+++ chap03/Operator.java
@@ -103,3 +103,13 @@ class Operator8 {
         System.out.println(c);
     }
 }
+
+// 7. Math.round()
+class Operator9 {
+    public static void main(String[] args) {
+        double pi = 3.141592;
+        double shortPi = Math.round(pi * 1000) / 1000.0;
+
+        System.out.println(shortPi);
+    }
+}
