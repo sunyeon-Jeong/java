738eb5553f9e58bd0816aad2cc92f0e53ace7359
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-15 17:38:35 +0900

    Feat : chap04(ConditionalStatement)_조건문 #5 - if문

---
+++ chap04/ConditionalStatement.java
@@ -0,0 +1,10 @@
+// 1. if문
+class ConditionalStatement1 {
+    public static void main(String[] args) {
+        int score = 80;
+
+        if (score > 60) {
+            System.out.println("합격");
+        }
+    }
+}
