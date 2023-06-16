a4ca647d5277cd3cfb28efef8b323340519ca3c8
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-16 16:46:25 +0900

    Feat : chap04(Iteration)_반복문 #6 - for문

---
+++ chap04/Iteration.java
@@ -0,0 +1,8 @@
+// 1. for문
+class Iteration1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Hello, Mallang!");
+        }
+    }
+}
