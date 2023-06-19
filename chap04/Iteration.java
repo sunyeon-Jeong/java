f77ee70bab0ef86e2c88d5690ae087d82c34119f
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-19 18:31:52 +0900

    Feat : chap04(Iteration)_반복문 #6 - 이름붙은반복문

--- chap04/Iteration.java
+++ chap04/Iteration.java
@@ -171,3 +171,61 @@ class Iteration10 {
         }
     }
 }
+
+class Iteration11 {
+    public static void main(String[] args) {
+        int menu = 0, num = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        // while문 : outer
+        outer:
+        while (true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.print("원하는 메뉴 (1 ~ 3)를 선택하시오 (종료 : 0) >>> ");
+
+            String userData = scanner.nextLine();
+            menu = Integer.parseInt(userData);
+
+            if (menu == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (! (1 <= menu && menu <= 3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다. (종료 : 0)");
+                continue;
+            }
+
+            // 무한반복문
+            for (;;) {
+                System.out.print("계산할 값을 입력하시오 (계산종료 : 0, 전체종료 : 99) >>> ");
+
+                userData = scanner.nextLine();
+                num = Integer.parseInt(userData);
+
+                if (num == 0) {
+                    break; // for문만 벗어남
+                }
+
+                if (num == 9) {
+                    break outer; // for문, while문 모두 벗어남
+                }
+
+                switch (menu) {
+                    case 1:
+                        System.out.println("result = " + num * num);
+                        break;
+
+                    case 2:
+                        System.out.println("result = " + Math.sqrt(num));
+                        break;
+
+                    case 3:
+                        System.out.println("result = " + Math.log(num));
+                        break;
+                }
+            }
+        }
+    }
+}
