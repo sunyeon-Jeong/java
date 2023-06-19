9d593387bedd72afc01500d60c38807f41af1b89
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-19 14:38:57 +0900

    Feat : chap04(Iteration)_반복문 #6 - 중첩for문

--- chap04/Iteration.java
+++ chap04/Iteration.java
@@ -32,3 +32,15 @@ class Iteration3 {
         }
     }
 }
+
+// 2. 중첩 for문
+class Iteration4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
