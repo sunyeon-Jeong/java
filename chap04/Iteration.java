299adc7a2dcc47c248105a90e05f882aba723e25
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-19 15:09:13 +0900

    Feat : chap04(Iteration)_반복문 #6 - while문

--- chap04/Iteration.java
+++ chap04/Iteration.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // 1. for문
 class Iteration1 {
     public static void main(String[] args) {
@@ -54,5 +56,29 @@ class Iteration5 {
             // 조건식에서 평가된 후 -> 1감소하여 i 출력됨
             System.out.println(i + " - I can do it!");
         }
+    }
+}
+
+// 사용자 입력숫자 각자리 합 구하기
+class Iteration6 {
+    public static void main(String[] args) {
+        int num = 0, sum = 0;
+
+        System.out.print("숫자를 입력하시오 (ex. 12345) >>> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userData = scanner.nextLine();
+
+        num = Integer.parseInt(userData);
+
+        // num % 10 -> 마지막자리 출력
+        // num / 10 -> 마지막자리 제거
+        while (num != 0) {
+            sum += num % 10;
+            System.out.printf("sum = %3d num = %d%n", sum, num);
+
+            num /= 10;
+        }
+        System.out.println("각 자리수 합 : " + sum);
     }
 }
