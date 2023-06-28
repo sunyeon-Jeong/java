410790109786953ec5b4da5cd941fb41d548f7cb
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-28 11:43:11 +0900

    Feat : chap05(Array)_배열 #7 - 배열출력_Refactoring

--- chap05/Array.java
+++ chap05/Array.java
@@ -8,7 +8,7 @@ class Array1 {
         int[] iArray1 = new int[10]; // 0 ~ 9
         int[] iArray2 = new int[10]; // 0 ~ 9
 
-        int[] iArray3 = {100, 95, 80, 70, 60};
+        int[] iArray3 = {100, 95, 80, 70, 60}; // 배열의초기화
 
         char[] chArray = {'a', 'b', 'c', 'd'};
 
@@ -23,12 +23,16 @@ class Array1 {
         for (int i = 0; i < iArray1.length; i++) {
             System.out.print(iArray1[i] + ",");
         }
+        System.out.println();
 
         System.out.println(Arrays.toString(iArray2));
+        // Arrays.toString() -> 배열의모든요소 String type으로 반환
         System.out.println(Arrays.toString(iArray3));
         System.out.println(Arrays.toString(chArray));
 
+        // 배열 바로 출력 -> 참조변수 반환
         System.out.println(iArray3);
+        // char type 배열 바로 출력 -> 구분자없이 배열의모든요소 예외출력
         System.out.println(chArray);
     }
 }
