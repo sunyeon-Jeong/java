f8c611976695de4eb44ffb3e6b3452d4f3191927
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-29 16:02:01 +0900

    Feat : chap06(Object-oriented Programming)_객체지향① #8 - 객체의생성과사용_공통class생성

--- chap06/Object.java
+++ chap06/Object.java
@@ -1,5 +1,15 @@
 // 객체지향(1)
 
-class Object1 {
+// #1. 객체의 생성과 사용
+// TV 클래스
+class Tv {
+    // 멤버변수 (속성)
+    String color; // null 초기화
+    boolean power; // false 초기화
+    int channel; // 0 초기화
 
+    // method(기능)
+    void power() {power = !power;}
+    void channelUp() {++channel;}
+    void channelDown() {--channel;}
 }
