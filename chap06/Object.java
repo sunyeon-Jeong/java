f27c8084079369d138db12edba23338b0707cd3d
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-06-30 17:32:24 +0900

    Feat : chap06(Object-oriented Programming)_객체지향① #8 - 클래스변수와인스턴스변수

--- chap06/Object.java
+++ chap06/Object.java
@@ -56,3 +56,35 @@ class Card {
     static int width = 100; // 멤버변수_클래스변수
     static int height = 250; // 멤버변수_클래스변수
 }
+
+class Object3 {
+    public static void main(String[] args) {
+        // 클래스변수 -> 객체생성없이 바로 사용가능
+        System.out.println("Card.width = " + Card.width);
+        System.out.println("Card.height = " + Card.height);
+
+        // 인스턴스변수 -> 선 객체 생성 후, 멤버변수 사용가능
+        // 객체생성 후, 인스턴스변수 값저장
+        Card card1 = new Card();
+        card1.kind = "Heart";
+        card1.number = 7;
+
+        Card card2 = new Card();
+        card2.kind = "Spade";
+        card2.number = 4;
+
+        System.out.println("card1은 " + card1.kind + ", " + card1.number + "이며, 크기는 ("
+                + card1.width + ", " + card1.height + ")");
+        System.out.println("card2는 " + card2.kind + ", " + card2.number + "이며, 크기는 ("
+                + card2.width + ", " + card2.height + ")");
+
+        System.out.println("card1의 width와 height를 각각 50, 80으로 변경합니다.");
+        card1.width = 50;
+        card1.height = 80;
+
+        System.out.println("card1은 " + card1.kind + ", " + card1.number + "이며, 크기는 ("
+                + card1.width + ", " + card1.height + ")");
+        System.out.println("card2는 " + card2.kind + ", " + card2.number + "이며, 크기는 ("
+                + card2.width + ", " + card2.height + ")");
+    }
+}
