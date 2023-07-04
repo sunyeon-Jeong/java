91d9ebf46a9e68910f865c81757a3d42d117fcd6
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-04 12:08:20 +0900

    Feat : chap06(Object-oriented Programming)_객체지향① #8 - 기본생성자

--- chap06/Object.java
+++ chap06/Object.java
@@ -275,3 +275,10 @@ class Data_2 {
     // 기본생성자
     Data_2() {}
 }
+
+class Object10 {
+    public static void main(String[] args) {
+        Data_1 data_1 = new Data_1();
+        Data_2 data_2 = new Data_2();
+    }
+}
